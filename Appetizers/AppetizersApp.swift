671f663dc2c8e60c2754421dcdcf8d9f1b558087
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-12-21 23:49:19 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Divyansh Bhardwaj on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
